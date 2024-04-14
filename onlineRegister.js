60f2a7d2ace1bd9ec7f660349c44ff173d9e8f76
P.S.H. Gunasekara <164228734+SajanHirusha@users.noreply.github.com>
2024-04-14 13:53:07 +0530

    js edit

--- onlineRegister.js
+++ onlineRegister.js
@@ -9,40 +9,6 @@ function closeNav() {
 
 
 let width = screen.width;  
-console.log(width);         
-let select1 = document.getElementById('sType');
-select1.addEventListener('change', function () {
-    loadForm(select1.value);
-});
-
-
-
-function loadForm(selectValue) {
-        if (selectValue === "onlineStudentsLogin") {
-
-            document.getElementById('display1').style.display = "initial";
-            document.getElementById('display2').style.display = "none";
-            document.getElementById('display3').style.display = "none";
-        }
-        else if (selectValue === "onlineStudentsRegister") {
-
-            document.getElementById('display2').style.display = "initial";
-            document.getElementById('display1').style.display = "none";
-            document.getElementById('display3').style.display = "none";
-        }
-        else if (selectValue === "internalStudents") {
-            document.getElementById('display3').style.display = "initial";
-            document.getElementById('display1').style.display = "none";
-            document.getElementById('display2').style.display = "none";
-        } else {
-            document.getElementById('display2').style.display = "none";
-            document.getElementById('display1').style.display = "none";
-            document.getElementById('display3').style.display = "none";
-        }
-}
-
-
-
 var form1 = document.getElementById("formDiv1");
 var form2 = document.getElementById("formDiv2");
 var form3 = document.getElementById("formDiv3");
@@ -52,19 +18,21 @@ var next2 = document.getElementById("next2");
 var back1 = document.getElementById("back1");
 var back2 = document.getElementById("back2");
 
+
+
 var progress = document.getElementById("progress");
 
 
 next1.onclick = function () {
-    if (width > 1230) {
+ if(width>550 && width <= 760){
         form1.style.left = "-450px";
         form2.style.left = "17px";
         progress.style.width = "240px";
-    } else if (width > 920 && width <= 1230) {
+    }else if(width>370 && width<= 550){
         form1.style.left = "-450px";
         form2.style.left = "7px";
         progress.style.width = "200px";
-    } else if(width>760 && width <= 920){
+    }else if(width<=370){
         form1.style.left = "-450px";
         form2.style.left = "7px";
         progress.style.width = "160px";
@@ -72,15 +40,15 @@ next1.onclick = function () {
 
 }
 back1.onclick = function () {
-    if (width > 1230) {
+ if(width>550 && width <= 760){
         form1.style.left = "17px";
         form2.style.left = "450px";
         progress.style.width = "120px";
-    } else if (width > 920 && width <= 1230) {
+    }else if(width>370 && width<= 550){
         form1.style.left = "7px";
         form2.style.left = "450px";
         progress.style.width = "100px";
-    } else if(width>760 && width <= 920){
+    }else if(width<=370){
         form1.style.left = "7px";
         form2.style.left = "450px";
         progress.style.width = "80px";
@@ -89,15 +57,16 @@ back1.onclick = function () {
 }
 
 next2.onclick = function () {
-    if (width > 1230) {
+    
+    if(width>550 && width <= 760){
         form2.style.left = "-450px";
         form3.style.left = "17px";
         progress.style.width = "360px";
-    } else if (width > 920 && width <= 1230){
+    }else if(width>370 && width<= 550){
         form2.style.left = "-450px";
         form3.style.left = "7px";
         progress.style.width = "300px";
-    } else if(width>760 && width <= 920){
+    }else if(width<=370){
         form2.style.left = "-450px";
         form3.style.left = "7px";
         progress.style.width = "240px";
@@ -105,20 +74,18 @@ next2.onclick = function () {
 }
 
 back2.onclick = function () {
-    if (width > 1230) {
+  if(width>550 && width <= 760){
         form2.style.left = "17px";
         form3.style.left = "450px";
         progress.style.width = "240px";
-    } else if (width > 920 && width <= 1230){
+    }else if(width>370 && width<= 550){
         form2.style.left = "7px";
         form3.style.left = "450px";
         progress.style.width = "200px";
-    } else if(width>760 && width <= 920){
+    }else if(width<=370){
         form2.style.left = "7px";
         form3.style.left = "450px";
         progress.style.width = "160px";
     }
+
 }
-
-
-
